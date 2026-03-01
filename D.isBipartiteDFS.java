1a0ba04b63da5b1495be9a451e8e2d613120bd9b
Muttu R K <kagalmutturaj@gmail.com>
2026-03-01 16:20:51 +0530

    Update and rename isBipartiteDFS.java to D.isBipartiteDFS.java

--- D.isBipartiteDFS.java
+++ D.isBipartiteDFS.java
@@ -1,4 +1,10 @@
-  public boolean isBipartite(int[][] graph) {
+
+https://leetcode.com/problems/is-graph-bipartite/description/
+Input: graph = [[1,2,3],[0,2],[0,1,3],[0,2]]
+Output: false
+Explanation: There is no way to partition the nodes into two independent sets such that every edge connects a node in one and a node in the other.
+
+public boolean isBipartite(int[][] graph) {
         int[] color = new int[graph.length];
         Arrays.fill(color, -1);
         for (int i = 0; i < graph.length; i++) {
